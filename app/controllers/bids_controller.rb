2586e32f7db9fab4df245de12e7be91c30d3a700
Josh McKelvey <jmckelvey0@gmail.com>
2015-10-15 02:13:42 -0400

    I dunno, I'm just commiting what I ended up with

--- app/controllers/bids_controller.rb
+++ app/controllers/bids_controller.rb
@@ -1,14 +1,18 @@
 class BidsController < ApplicationController
 
   def create
-    @bid = Bid.new(bid_params)
-
-    if @bid.save
-      redirect_to @bid.item, notice: 'Your bid has been recorded!'
-    else
-      @item = @bid.item
-      render "items/show"
+    @bid = Bid.create!(bid_params)
+    redirect_to @bid.item do |format|
+      format.html
+      format.ajax { dataType 'html'}
     end
+
+    # if @bid.save
+    #   redirect_to @bid.item, notice: 'Your bid has been recorded!'
+    # else
+    #   @item = @bid.item
+    #   render "items/show"
+    # end
   end
 
 
